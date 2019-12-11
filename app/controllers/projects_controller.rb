35473d6385b60e121115d480d7ed12b7e9e6d0bd
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-12-10 22:33:13 -0800

    Add basic site layout

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,20 +1,23 @@
 class ProjectsController < ApplicationController
-  def create
+
+  def index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def edit
   end
 
-  def update
-  end
-
-  def index
-  end
-
   def delete
+  end
+
+  def create
+  end
+
+  def update
   end
 
   def destroy
