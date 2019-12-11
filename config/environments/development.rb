755e65485d62b722df083624e1dab3055fffd170
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-12-11 08:09:10 -0800

    Send from real email in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'joshuahunschejones@gmail.com' }
+  config.action_mailer.default_options = { from: 'joshua@hunschejones.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
@@ -52,7 +52,7 @@ Rails.application.configure do
     domain:               'localhost:3000',
     user_name:            ENV['EMAIL_USERNAME'],
     password:             ENV['EMAIL_PASSWORD'],
-    authentication:       'plain',
+    authentication:       :plain,
     enable_starttls_auto: true
   }
 
