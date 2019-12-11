6bc9f64a1dba2186c8c9c5e0233abc6ce570b42c
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-12-10 22:33:13 -0800

    Add basic site layout

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :users
-  has_many :tracks, :events, :download_links, :comments
+  has_many :tracks
+  has_many :events
+  has_many :download_links
+  has_many :comments
 end
