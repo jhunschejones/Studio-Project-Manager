562f97a225fd40e73854280af42d5fbec197326f
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-12-11 07:14:51 -0800

    Use login email authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # https://help.hover.com/hc/en-us/articles/217281777-Email-server-settings
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
+    address:              'mail.hover.com',
     port:                 465,
     domain:               'hunschejones.com',
-    user_name:            ENV['SENDGRID_PASSWORD'],
-    password:             ENV['SENDGRID_USERNAME'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    user_name:            ENV['EMAIL_USERNAME'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       :login,
+    ssl: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
