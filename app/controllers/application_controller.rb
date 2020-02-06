9f36520ee153a8629e39a652fd00972827b70103
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-02-05 16:50:10 -0800

    Set sign out path to avoid flash on user logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
     @project ||= Project.includes(:tracks, :links, :users, :events, :notifications).find(params[:project_id] || params[:id])
     redirect_to projects_path, notice: "You cannot access that project" unless current_user.can_access_project?(@project)
   end
+
+  # Overwriting the sign_out redirect path method in devise
+  # https://github.com/heartcombo/devise/wiki/How-To:-Change-the-redirect-path-after-destroying-a-session-i.e.-signing-out
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
