b11e113cb4edfccb18ec6ab721de4c985cff5507
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-06-30 11:42:12 -0700

    Add event and comment model tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
+# bundle exec ruby -Itest test/models/comment_test.rb
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe "notify_project_users" do
+    test "enqueues a notification job after create" do
+      stubbed_job = mock()
+      stubbed_job.stubs(:perform_later)
+      NotifyOnCommentJob.expects(:set).times(1).returns(stubbed_job)
+      stubbed_job.expects(:perform_later).times(1).with(kind_of(Integer), "added")
+
+      Comment.create(user: users(:one), commentable: track_versions(:one))
+    end
+  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
+# bundle exec ruby -Itest test/models/event_test.rb
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe "notify_project_users" do
+    test "enqueues a notification job after create" do
+      stubbed_job = mock()
+      stubbed_job.stubs(:perform_later)
+      NotifyOnEventJob.expects(:set).times(1).returns(stubbed_job)
+      stubbed_job.expects(:perform_later).times(1).with(kind_of(Integer), "added")
+
+      Event.create(title: "Drum Tracking",
+        start_at: Time.now,
+        end_at: Time.now + 5.hours,
+        project: projects(:one),
+        user: users(:one)
+      )
+    end
+  end
 end
