3b3f16bf17f48e7ca414e15040543f31ccd0ed00
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-01-26 11:15:42 -0800

    Fix rout inclusion bug, update to Rails 6.1.1
    - Rails.application.routes.url_helpers wasn't included correctly in the helper module, causing explosions when upgrading past Rails 6.0.3.x

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,22 +1,24 @@
-include Rails.application.routes.url_helpers
-
 module ProjectsHelper
-  def self.breadcrumbs_for(opts={})
-    breadcrumbs = [{text: "Projects", path: projects_path}]
+  class << self
+    include Rails.application.routes.url_helpers
 
-    project = opts[:project]
-    event = opts[:event]
-    track = opts[:track]
-    track_version = opts[:track_version]
-    page_title = opts[:page_title]
+    def breadcrumbs_for(opts={})
+      breadcrumbs = [{text: "Projects", path: projects_path}]
 
-    breadcrumbs.push({text: project.title, path: project_path(project)}) if project
-    breadcrumbs.push({text: event.title, path: project_event_path(project, event)}) if project && event
-    breadcrumbs.push({text: track.title, path: project_track_path(project, track)}) if project && track
-    breadcrumbs.push({text: track_version.title, path: project_track_track_version_path(project, track, track_version)}) if project && track && track_version
+      project = opts[:project]
+      event = opts[:event]
+      track = opts[:track]
+      track_version = opts[:track_version]
+      page_title = opts[:page_title]
 
-    breadcrumbs.push({text: page_title[:text], path: ""}) if project && page_title
+      breadcrumbs.push({text: project.title, path: project_path(project)}) if project
+      breadcrumbs.push({text: event.title, path: project_event_path(project, event)}) if project && event
+      breadcrumbs.push({text: track.title, path: project_track_path(project, track)}) if project && track
+      breadcrumbs.push({text: track_version.title, path: project_track_track_version_path(project, track, track_version)}) if project && track && track_version
 
-    breadcrumbs
+      breadcrumbs.push({text: page_title[:text], path: ""}) if project && page_title
+
+      breadcrumbs
+    end
   end
 end
